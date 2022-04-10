581f81d1abc5aad2e317770a54fee76466eb1688
Erik C. Forés <soc@erik.cat>
2022-04-10 19:59:53 +0200

    add xml

--- src/components/code.jsx
+++ src/components/code.jsx
@@ -2,8 +2,10 @@ import hljs from 'highlight.js/lib/core'
 import 'highlight.js/styles/xcode.css'
 import { onMount } from 'solid-js'
 import js from 'highlight.js/lib/languages/javascript'
+import xml from 'highlight.js/lib/languages/xml'
 
-hljs.registerLanguage('jsx', js)
+hljs.registerLanguage('javascript', js)
+hljs.registerLanguage('xml', xml)
 
 const Code = (props) => {
   let element = undefined
